d5de4fd1825822435f948fc3847623edf6e1f78d
Mikołaj Rozwadowski <mikolaj.rozwadowski@outlook.com>
2017-05-19 19:34:12 +0200

    Create the secant solver

---
+++ solvers/secant.cpp
@@ -0,0 +1,37 @@
+#include "secant.h"
+#include <cmath>
+#include <algorithm>
+
+long double FloatingPoint::Secant(long double a, long double b, Function *func) {
+    long double fa, fb, h, x, y;
+
+    h = 0.179372 * (b - a);
+    a = a + h;
+    b = b - h;
+    fa = func->evaluate(a);
+    fb = func->evaluate(b);
+
+    if (fabs(fa) < fabs(fb)) {
+        std::swap(a, b);
+        std::swap(fa, fb);
+    }
+
+    while (true) {
+        x = fa - fb;
+        if (x == 0) {
+            return b;
+        }
+
+        h = b - a;
+        x = b + fb*h/x;
+        y = func->evaluate(x);
+        if (x == a || x == b || y == 0) {
+            return x;
+        }
+
+        fa = fb;
+        fb = y;
+        a = b;
+        b = x;
+    }
+}
---
+++ solvers/secant.h
@@ -0,0 +1,10 @@
+#ifndef SECANT_H
+#define SECANT_H
+
+#include "common.h"
+
+namespace FloatingPoint {
+    long double Secant(long double a, long double b, Function *func);
+}
+
+#endif // SECANT_H
